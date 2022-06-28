35cedd81826257dc8833c71f3fcb8ea51b6d6f52
AntonioHincapie <marcohincapiem@gmail.com>
2022-06-28 09:56:26 -0500

    Dispatch the categories action

--- src/components/categories.js
+++ src/components/categories.js
@@ -1,12 +1,21 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Check } from '../redux/categories/categories';
 import NavBar from './navbar';
 
 export default function Categories() {
+  const categories = useSelector((state) => state.categories);
+  const dispatch = useDispatch();
+
+  const StatusHandler = () => {
+    dispatch(Check());
+  };
+
   return (
     <div>
       <NavBar />
-      <h2>Categories</h2>
-      <button type="button" className="book-categories">Check Status</button>
+      <h2>{categories}</h2>
+      <button type="button" onClick={StatusHandler}>Check Status</button>
     </div>
   );
 }
