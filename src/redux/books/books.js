7330b3a806251b9ba896ae0e21a47e6f52cefc01
AntonioHincapie <marcohincapiem@gmail.com>
2022-06-29 16:09:40 -0500

    Refactoring the Redux book.js file

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,28 +1,12 @@
-import { v4 as uuidv4 } from 'uuid';
-
+const URL = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/m8Y4ZPtAHK2eZguBBm1C/books';
 const ADDBOOK = 'bookstore-project/books/ADDBOOK';
 const DELETEBOOK = 'bookstore-project/books/DELETEBOOK';
+const GETBOOK = 'bookstore-project/books/GETBOOK';
 
-const initialState = [
-  {
-    id: uuidv4(),
-    title: 'Hacking con Python',
-    author: 'Daniel Echeverri Montoya',
-  },
-  {
-    id: uuidv4(),
-    title: 'Hacker Épico',
-    author: 'Alejandro Ramos - Rodrigo Yepes',
-  },
-  {
-    id: uuidv4(),
-    title: '100 años de soledad',
-    author: 'Gabriel Garcia Marquez',
-  },
-];
-
-export default function BookReducer(state = initialState, action = {}) {
+export default function BookReducer(state = [], action = {}) {
   switch (action.type) {
+    case GETBOOK:
+      return [...action.books]
     case ADDBOOK:
       return [...state,
         { title: action.book.title, author: action.book.author, id: action.book.id },
@@ -34,12 +18,75 @@ export default function BookReducer(state = initialState, action = {}) {
   }
 }
 
-export const AddBook = (title, author) => ({
+const AddBook = (book) => ({
   type: ADDBOOK,
-  book: { title, author, id: uuidv4() },
+  book,
 });
 
-export const BookToErase = (id) => ({
+const BookToErase = (id) => ({
   type: DELETEBOOK,
   bookToErase: id,
 });
+
+const GetBook = (books) => ({
+  type: GETBOOK,
+  books,
+})
+
+export const FetchBook = () => (dispatch) => {
+  const List = [];
+  fetch(URL, {
+    headers: {
+      'Content-Type': 'application/json',
+    accept: 'application/json',
+    },
+  })
+  .then((response) => response.json())
+  .then((result) => {
+    const DataEntry = Object.entries(result);
+    DataEntry.forEach((value) => {
+      List.push({
+        title: value[1][0].title,
+        author: value[1][0].author,
+        category: value[1][0].category,
+        id: value[0],
+      });
+    });
+    dispatch(GetBook(List));
+  });
+};
+
+export const AddFetchBook = (book) => async (dispatch) => {
+  const post = await fetch(URL, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    accept: 'application/json',
+    },
+    body: JSON.stringify(
+      {
+        item_id: book.id,
+        title: book.title,
+        author: book.author,
+        category: '',
+      },
+    ),
+  });
+  if (post.status === 201) {
+    dispatch(AddBook(book));
+  }
+};
+
+export const DeleteFetchBook = (id) => async (dispatch) => {
+  const DelURL = `${URL}/${id}`;
+  const delFecthBook = await fetch(DelURL, {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json',
+    accept: 'application/json',
+    },
+  });
+  if (delFecthBook.status === 201) {
+    dispatch(BookToErase(id));
+  }
+};
